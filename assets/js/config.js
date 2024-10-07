aa8dc96a5d6a55468f19f55e19519b9a08eafde5
yugeshsivakumar <yugeshmng72003@gmail.com>
2024-10-07 22:20:48 +0530

    update

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+    const firebaseConfig = {
+      apiKey: "AIzaSyDk3ugH6c1TqesuMDoOMoovqhMziIuoc4c",
+      authDomain: "portfolio-936fd.firebaseapp.com",
+      projectId: "portfolio-936fd",
+      storageBucket: "portfolio-936fd.appspot.com",
+      messagingSenderId: "938063460628",
+      appId: "1:938063460628:web:777756afbb689396ac2a01",
+      measurementId: "G-BV0049HB4H"
+    };
